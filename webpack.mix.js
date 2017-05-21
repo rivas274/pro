73f3c7c038306fed2706c1d1cae5896d1fe2ca44
rivas274 <rivas274@outlook.com>
2017-05-21 15:39:38 -0400

    pueba git

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,23 @@
 const { mix } = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .extract(['vue','axios','jquery']).version()
+    .sass('resources/assets/sass/app.scss', 'public/css').version()
+    .browserSync('pro.app');
+/* para integrar estylos de una plantilla
+mix.styles([
+    'public/css/vendor/normalize.css',
+    'public/css/vendor/videojs.css'
+], 'public/css/all.css');
+*/
+/* para integrar js de una plantilla
+mix.scripts([
+    'public/js/admin.js',
+    'public/js/dashboard.js'
+], 'public/js/all.js');
+*/
+/* 
+mix.copy('node_modules/foo/bar.css', 'public/css/bar.css');
+mix.copyDirectory('assets/img', 'public/img'); 
+mix.disableNotifications();
+*/
