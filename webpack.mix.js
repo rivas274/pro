c77e8b58158876b26bfd0ac03810e527acd8a754
rivas274 <rivas274@outlook.com>
2017-05-22 19:22:13 -0400

    plantilla admin lte integrada

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,35 @@
 const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .extract(['vue','axios','jquery']).version()
-    .sass('resources/assets/sass/app.scss', 'public/css').version()
-    .browserSync('pro.app');
-/* para integrar estylos de una plantilla
-mix.styles([
-    'public/css/vendor/normalize.css',
-    'public/css/vendor/videojs.css'
-], 'public/css/all.css');
-*/
-/* para integrar js de una plantilla
-mix.scripts([
-    'public/js/admin.js',
-    'public/js/dashboard.js'
-], 'public/js/all.js');
-*/
-/* 
-mix.copy('node_modules/foo/bar.css', 'public/css/bar.css');
-mix.copyDirectory('assets/img', 'public/img'); 
-mix.disableNotifications();
-*/
+    //.extract(['vue','axios','jquery']).version()
+   .js('resources/assets/js/app-landing.js', 'public/js/app-landing.js')
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .less('node_modules/bootstrap-less/bootstrap/bootstrap.less', 'public/css/bootstrap.css')
+   .less('resources/assets/less/adminlte-app.less','public/css/adminlte-app.css')
+   .less('node_modules/toastr/toastr.less','public/css/toastr.css')
+   .combine([
+       'public/css/app.css',
+       'node_modules/admin-lte/dist/css/skins/_all-skins.css',
+       'public/css/adminlte-app.css',
+       'node_modules/icheck/skins/square/blue.css',
+       'public/css/toastr.css'
+   ], 'public/css/all.css')
+   .combine([
+       'public/css/bootstrap.css',
+       'resources/assets/css/main.css'
+   ], 'public/css/all-landing.css')
+   //APP RESOURCES
+   .copy('resources/assets/img/*.*','public/img')
+   //VENDOR RESOURCES
+   .copy('node_modules/font-awesome/fonts/*.*','public/fonts/')
+   .copy('node_modules/ionicons/dist/fonts/*.*','public/fonts/')
+   .copy('node_modules/admin-lte/bootstrap/fonts/*.*','public/fonts/bootstrap')
+   .copy('node_modules/admin-lte/dist/css/skins/*.*','public/css/skins')
+   .copy('node_modules/admin-lte/dist/img','public/img')
+   .copy('node_modules/admin-lte/plugins','public/plugins')
+   .copy('node_modules/icheck/skins/square/blue.png','public/css')
+   .copy('node_modules/icheck/skins/square/blue@2x.png','public/css')
+   .browserSync('pro.app');
+if (mix.config.inProduction) {
+  mix.version();
+}
